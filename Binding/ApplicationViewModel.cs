73a46cb145d9dbedbcaca664c02d238afe365066
MukIlnur <24mi@bk.ru>
2023-01-30 22:51:00 +0500

    Add project files.

---
+++ Binding/ApplicationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    internal class ApplicationViewModel 
+    {
+        public ObservableCollection<Phone> Phones { get; set; }    
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+        
+    }
+}
---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationViewModel avm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            avm = new ApplicationViewModel();
+            DataContext = avm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            avm.Phones.Add(new Phone { Title = "SonyX1", Company = "Sony", Price = 135000 });
+        }
+    }
+}
